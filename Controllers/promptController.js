7b54aa986cccc0355d362910287a7909bab3abf4
Mr. Hasan <hasanshahriar32@gmail.com>
2023-06-26 10:55:15 +0600

    added prompt controller for edge function

--- Controllers/promptController.js
+++ Controllers/promptController.js
@@ -1,7 +1,5 @@
-const OpenAiStream = require("./openAiStream");
 const asyncHandler = require("express-async-handler");
 const { Configuration, OpenAIApi } = require("openai");
-// const database = require("./database"); // Assuming you have a separate module for database operations
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,74 +7,29 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const config = {
-  runtime: "edge",
-};
-
 const generateResponse = asyncHandler(async (req, res) => {
-  const userPrompt = req.body;
-  const {
-    subjectSelection,
-    question,
-    sessionId,
-    additionalInstruction,
-    assistanceLevel,
-    uid,
-  } = userPrompt;
+  //   const userPrompt = req.body;
 
-  const responseStream = await OpenAiStream({
+  const response = await openai.createCompletion({
     model: "text-davinci-003",
+
     prompt: `
-      Subject: ${subjectSelection} ,
-      Topic or Question: ${question},
-      Assistance Level: ${assistanceLevel},
-      Additional Details: ${additionalInstruction},
-      
-      Act as a teaching professional and analyze the question or topic and generate a comprehensive response to assist. Please explain the concept and offer step-by-step guidance.
-      `,
+            Subject: Mathematics 
+            Topic or Question: How to solve quadratic equations?
+            Assistance Level: step-by-step guidance
+            Additional Details: I am struggling with factoring quadratic equations. Can you provide some examples to practice?
+            
+            Act as a teaching professional and analyze the question or topic and generate a comprehensive response to assist. Please explain the concept, and offer step-by-step guidance.
+            `,
     temperature: 0.1,
     max_tokens: 600,
-    stream: true,
-  });
-  const chunks = [];
-  const reader = responseStream.getReader();
-
-  // Send each chunk to the frontend using Server-Sent Events
-  res.writeHead(200, {
-    "Content-Type": "text/event-stream",
-    "Cache-Control": "no-cache",
-    Connection: "keep-alive",
   });
 
-  let index = 0;
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) break;
+  console.log(response.data.choices, "response");
 
-    // Process and save the chunk to the database asynchronously
-    const chunkText = new TextDecoder().decode(value);
-    // database.saveChunk(chunkText).catch((error) => {
-    //   console.error("Error saving chunk to database:", error);
-    // });
-
-    // Send the chunk to the frontend
-    const event = `data: ${chunkText}\n\n`;
-    res.write(event);
-
-    // Simulate a delay between sending chunks (adjust the delay as needed)
-    setTimeout(() => {
-      if (index === chunks.length - 1) {
-        res.write("event: end\n\n"); // Signal the end of the stream
-      }
-    }, index * 0); // Delay each chunk by 1 second
-
-    index++;
-  }
-
-  res.end();
+  res.send(response.data.choices);
 });
 
 module.exports = {
   generateResponse,
-  config,
 };
--- Routes/PromptRoute.js
+++ Routes/PromptRoute.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { generateResponse, config } = require("../Controllers/promptController");
+const {
+  generateResponse2,
+  config,
+} = require("../Controllers/promptController2");
+const { generateResponse } = require("../Controllers/promptController");
 
 // const {} = require("../Controllers/userController");
 
-router.post("/", generateResponse);
+router.post("/", generateResponse2);
+router.get("/prompt", generateResponse);
 
 module.exports = router;
