fa623f2ad7728e59fd62cddaf834e7e9c1e7acfc
Mr. Hasan <hasanshahriar32@gmail.com>
2023-06-28 10:01:25 +0600

    added all session details needed

--- Controllers/sessionController.js
+++ Controllers/sessionController.js
@@ -48,13 +48,42 @@ const allSession = asyncHandler(async (req, res) => {
     .skip(skipIndex)
     .limit(limit)
     .sort({ createdAt: -1 });
+  // .select("-messages");
   res.send(session);
 });
 
-// generate a response based on the user's prompt
-// do this after the user has created a session
+// get only the favorite sessions
+const favoriteSession = asyncHandler(async (req, res) => {
+  const sessionDetail = req.body;
+  const page = sessionDetail?.page ? parseInt(sessionDetail?.page) : 1;
+  const limit = sessionDetail?.limit ? parseInt(sessionDetail?.limit) : 50;
+  const uid = sessionDetail?.uid;
+  const skipIndex = (page - 1) * limit;
+  const session = await Session.find({ uid, isBookmarked: true })
+    .skip(skipIndex)
+    .limit(limit)
+    .sort({ createdAt: -1 })
+    .select("-messages");
+  res.send(session);
+});
+
+// get a single session
+const singleSession = asyncHandler(async (req, res) => {
+  const sessionDetail = req.body;
+  const sessionId = sessionDetail?.sessionId;
+
+  const session = await Session.findOne({ sessionId });
+  if (session) {
+    res.status(201).send(session);
+  } else {
+    res.status(400);
+    throw new Error("Invalid session data");
+  }
+});
 
 module.exports = {
   generateSession,
   allSession,
+  favoriteSession,
+  singleSession,
 };
--- Routes/SessionRoute.js
+++ Routes/SessionRoute.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const {
   generateSession,
   allSession,
+  favoriteSession,
+  singleSession,
 } = require("../Controllers/sessionController");
 
 router.post("/all", allSession);
+router.post("/favorite", favoriteSession);
+router.post("/single", singleSession);
 router.post("/", generateSession);
 
 module.exports = router;
