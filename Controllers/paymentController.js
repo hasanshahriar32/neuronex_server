7fc98bdaab39b53838e5b36fea5c43d05cc48ff8
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-07 04:44:09 +0600

    updated

--- Controllers/paymentController.js
+++ Controllers/paymentController.js
@@ -7,11 +7,11 @@ const createIntent = asyncHandler(async (req, res) => {
   //   const price = pricingData.find((p) => p._id === id).price;
   const price = 500;
   const amount = price * 100;
-  console.log(pricingData);
+  console.log(price);
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: amount,
-    currency: "bdt",
+    currency: "usd",
     automatic_payment_methods: {
       enabled: true,
     },
@@ -20,5 +20,38 @@ const createIntent = asyncHandler(async (req, res) => {
     clientSecret: paymentIntent.client_secret,
   });
 });
-
+const resolveIntent = asyncHandler(async (req, res) => {
+  const { packageId } = req.body;
+  // search for the package and get amount
+  const package = Package.findById(packageId);
+  // console.log(package);
+  // const amount = package.price;
+  const amount = 30;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "bdt",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+  const payment = await Payment.create({
+    paymentID: "",
+    package,
+    status: "pending",
+    clientSecret: paymentIntent.client_secret,
+  });
+  if (payment) {
+    res.status(201).json({
+      _id: payment._id,
+      paymentID: payment.paymentID,
+      package: payment.package,
+      status: payment.status,
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 module.exports = { createIntent };
