d816ab9ef6cb01e1933e1d3c11a041397ab528d2
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-10 03:51:08 +0600

    updated ai model

---
+++ Model/aiModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const aiSchema = mongoose.Schema(
+  {
+    initBalance: { type: Number, trim: true, required: true, default: 0 },
+    outPrice: { type: Number, trim: true, required: true, default: 0.002 },
+    inPrice: { type: Number, trim: true, required: true, default: 0.0015 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Ai = mongoose.model("Ai", aiSchema);
+
+module.exports = Ai;
